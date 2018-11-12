b3718de823171713506ec80e7d188454763191e9
Petr Vaněk <arkamar@atlas.cz>
2018-11-12 21:33:03 +0100

    smtpd: add few commands

--- hp-smtpd.c
+++ hp-smtpd.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct commands {
@@ -24,6 +25,18 @@ smtp_greet(char * code) {
 }
 
 void
+smtp_helo(char * arg) {
+	smtp_greet("250 ");
+	out("\r\n");
+}
+
+void
+smtp_ehlo(char * arg) {
+	smtp_greet("250-");
+	out("\r\n250-PIPELINING\r\n250 8BITMIME\r\n");
+}
+
+void
 err_unimpl(char * arg) {
 	out("502 unimplemented (#5.5.1)\r\n");
 }
@@ -33,13 +46,48 @@ smtp_rcpt(char * arg) {
 	out("250 ok\r\n");
 }
 
+static
+void
+smtp_rset(char * arg) {
+	out("250 flushed\r\n");
+}
+
+static
+void
+smtp_quit(char * arg) {
+	smtp_greet("221 ");
+	out("\r\n");
+	flush();
+	exit(0);
+}
+
 struct commands smtp_commands[] = {
 	{ "rcpt", smtp_rcpt, 0 },
+	{ "ehlo", smtp_ehlo, flush },
+	{ "quit", smtp_quit, flush },
+	{ "helo", smtp_helo, flush },
+	{ "rset", smtp_rset, 0 },
 	{ NULL, err_unimpl, flush }
 };
 
+
 int
 main(int argc, char * argv[]) {
+	char * line = NULL;
+	size_t cap = 0;
+	ssize_t len;
+	int i;
+
 	smtp_greet("200 ");
 	out(" ESMTP\r\n");
+	while ((len = getline(&line, &cap, stdin)) > 0) {
+		for (i = 0; smtp_commands[i].cmd; i++) {
+			if (!strncasecmp(line, smtp_commands[i].cmd, 4)) {
+				smtp_commands[i].fun(line);
+				if (smtp_commands[i].flush)
+					smtp_commands[i].flush();
+			}
+		}
+	}
+	free(line);
 }
